dbf4ad2d10dd30652d562c0413230378080bea16
Gregory S. Kielian <gregory.kielian@gmail.com>
2016-07-03 18:00:09 -0700

    created clean examples for monitoring and adding data

---
+++ public/js/firebase-init.js
@@ -0,0 +1,9 @@
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyD279kzEnIoSjI-R8W9dGsuTL09skTvFJQ",
+        authDomain: "crackling-heat-8070.firebaseapp.com",
+        databaseURL: "https://crackling-heat-8070.firebaseio.com",
+        storageBucket: "crackling-heat-8070.appspot.com",
+  };
+
+firebase.initializeApp(config);
